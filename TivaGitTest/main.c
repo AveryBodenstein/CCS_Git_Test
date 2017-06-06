0049b7e23aa8a1e344698ea6ce2fcca333ebbb0d
Avery <AveryBodenstein@users.noreply.github.com>
2017-06-06 11:36:19 -0400

    added init stub

--- TivaGitTest/main.c
+++ TivaGitTest/main.c
@@ -3,10 +3,17 @@
  */
 #include <stdint.h>
 
+void init();
+
 // Test Git Comment
 int main(void) {
 	uint16_t test;
+	init();
 	while(1){
 		test++;
 	}
+}
+
+void init(){
+	// this function initializes things
 }
